7ed915149e020f4e4f954b26b86fc13f868809ee
NghiNV <nghinv@lumi.biz>
2021-06-08 18:13:08 +0700

    Fix typing

--- react-native-services.d.ts
+++ react-native-services.d.ts
@@ -1,16 +1,30 @@
-import { ActionSheetServiceType } from '@nghinv/react-native-action-sheet';
-import { BottomSheetServiceType } from '@nghinv/react-native-bottom-sheet';
-import { AlertServiceType } from '@nghinv/react-native-alert';
-import { DropdownAlertServiceType } from '@nghinv/react-native-dropdown-alert';
-import { SpinnerProps } from '@nghinv/react-native-loading';
-import React from 'react';
+declare module '@nghinv/react-native-services' {
+  import React from 'react';
+  import { ActionSheetServiceType, ActionSheetViewProps } from '@nghinv/react-native-action-sheet';
+  import { BottomSheetServiceType, BottomSheetViewProps } from '@nghinv/react-native-bottom-sheet';
+  import { AlertServiceType, AlertViewProps } from '@nghinv/react-native-alert';
+  import { DropdownAlertServiceType, DropdownAlertViewProps } from '@nghinv/react-native-dropdown-alert';
+  import { SpinnerProps, LoadingViewProps } from '@nghinv/react-native-loading';
 
-export interface Services {
-  ActionSheet: ActionSheetServiceType;
-  BottomSheet: BottomSheetServiceType;
-  Alert: AlertServiceType;
-  DropdownAlert: DropdownAlertServiceType;
-  Loading: SpinnerProps;
+  interface ServicesType {
+    ActionSheet: ActionSheetServiceType;
+    BottomSheet: BottomSheetServiceType;
+    Alert: AlertServiceType;
+    DropdownAlert: DropdownAlertServiceType;
+    Loading: SpinnerProps;
+  }
+
+  interface ServiceProviderProps {
+    actionSheetProps?: ActionSheetViewProps;
+    bottomSheetProps?: BottomSheetViewProps;
+    alertProps?: AlertViewProps;
+    dropdownAlertProps?: DropdownAlertViewProps;
+    loadingProps?: LoadingViewProps;
+  }
+
+  export const Services: ServicesType;
+
+  export class ServicesProvider extends React.PureComponent<ServiceProviderProps> {
+
+  }
 }
-
-export interface ServicesProvider extends React.Component { }
